145d1adff6ec38684f3abe23c4cdec35fd81a57e
Yunnniii <yunnniii@yun-yeojun-ui-MacBookPro.local>
2022-08-16 21:59:21 +0900

    java study

---
+++ src/main/java/org/opentutorials/javatutorials/datatype/IntDataTypeDemo.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorials.datatype;
+
+public class IntDataTypeDemo {
+    public static void main(String[] args) {
+        short a;
+        a = 128;
+        //byte population = 500000000;
+        int population = 500000000;
+    }
+}
